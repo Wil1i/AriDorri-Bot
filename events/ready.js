d62f8653cddd494f6cf312c4b57e043b18d5f2b8
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-07-08 16:52:32 +0430

    Ready event synced

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const index = require("../index")
+const commandHandler = require("../helpers/commandRegister")
 const config = require("../configs/config.json")
 const setActivity = require("../helpers/setActivity")
 
@@ -6,13 +6,12 @@ module.exports = {
     name : "ready",
     description : "ready handler (for when bot goes online)",
     async execute(client){
-
         console.log(`Bot [${client.user.tag}] is now ready to use.`)
         setActivity(client)
 
         const guild = client.guilds.cache.get(config.guilds.main)
         const adminGuild = client.guilds.cache.get(config.guilds.admin)
-        await guild.commands.set(index.commands)
-        await adminGuild.commands.set(index.commands)
+        await guild.commands.set(commandHandler.commands)
+        await adminGuild.commands.set(commandHandler.commands)
     }
 }
