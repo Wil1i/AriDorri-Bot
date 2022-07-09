c7719299596a94e11661758e59463454c674d063
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-07-09 13:06:59 +0430

    suggest enabled for mention

---
+++ events/messageCreate.js
@@ -0,0 +1,14 @@
+const Suggest = require("../models/Suggest")
+
+module.exports = {
+    name : "messageCreate",
+    async execute(client, message){
+        const firstMention = message.mentions.users.first()
+        if (firstMention.id == client.user.id){
+            await Suggest.create({
+                name : message.author.tag,
+                text : message.content
+            })
+        }
+    }
+}
