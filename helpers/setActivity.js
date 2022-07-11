76f0496d8bdef6602bdbef1b610e0f92db7dd445
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-07-11 21:04:45 +0430

    fix

--- helpers/setActivity.js
+++ helpers/setActivity.js
@@ -19,6 +19,10 @@ const activity = async (client) => {
         }
     })
 
+    client.user.setActivity(activityText.value, {
+        type : activityMode.value.toUpperCase(),
+        url : activityURL.value
+    })
     setInterval(() => {
         client.user.setActivity(activityText.value, {
             type : activityMode.value.toUpperCase(),
