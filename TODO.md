4a5f4a1e272f8872c7228d93292e4385fb1882ec
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-09-01 00:30:33 +0430

    New youtube video notification added

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
-- [ ] a event listener on message for when a youtube video published and notification sent into discord channel, it's automaticly send it into twitch chat (or if its not possible create a database for it and handle it on python side)
+- [x] a event listener on message for when a youtube video published and notification sent into discord channel, it's automaticly send it into twitch chat (or if its not possible create a database for it and handle it on python side)
 
 - [ ] Move anything we have in config file into web
 
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,14 +1,34 @@
-const Suggest = require("../models/Suggest")
+const Suggest = require("../models/Suggest");
+const twitchClient = require("../helpers/twitchClientHandler");
 
 module.exports = {
-    name : "messageCreate",
-    async execute(client, message){
-        const firstMention = message.mentions.users.first()
-        if (firstMention && firstMention.id == client.user.id){
-            await Suggest.create({
-                name : message.author.tag,
-                text : message.content
-            })
-        }
+  name: "messageCreate",
+  async execute(client, message) {
+    const firstMention = message.mentions.users.first();
+    if (firstMention && firstMention.id == client.user.id) {
+      await Suggest.create({
+        name: message.author.tag,
+        text: message.content,
+      });
     }
-}
+
+    // ? check for youtube content, if a new content released, send news to twitch chat
+    if (message.channel.id == "666293441340112925") {
+      const isMessageHaveLink = message.content.includes("https://");
+      if (isMessageHaveLink) {
+        const messageSplit = message.content.split(" ");
+        let link = null;
+        for (msg of messageSplit) {
+          if (msg.startsWith("https") && msg.includes("youtu")) {
+            link = msg;
+            break;
+          }
+        }
+
+        if (link) {
+          twitchClient.say("aridorri", `New YouTube video released : ${link}`);
+        }
+      }
+    }
+  },
+};
